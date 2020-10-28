c08f81fd0269aac8cf501b7221f227f222e4d1c6
Gabriel Feitosa <gabriel.perino99@gmail.com>
2020-10-28 10:54:06 -0300

    updated car app using revealing module pattern notation

--- 29-javascript-gabriel-feitosa.js
+++ 29-javascript-gabriel-feitosa.js
@@ -1,7 +1,7 @@
-(function (DOM,win,doc) {
-    'use strict';
-  
-    /*
+(function (DOM, win, doc) {
+  "use strict";
+
+  /*
     Vamos estruturar um pequeno app utilizando módulos.
     Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
     A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
@@ -31,69 +31,74 @@
     que será nomeado de "app".
     */
 
-    var ajax = new XMLHttpRequest()
-    var $companyName=  DOM('[data-js=companyName]')
-    var $companyPhone=  DOM('[data-js=companyPhone]')
-    var $carForm =  DOM('[data-js=carForm]')
-
-    $carForm.on('submit', handleSubmit)
-
-    function handleSubmit(event){
-        event.preventDefault()
-        var $carTable = DOM('[data-js=carTable]')
-        console.log($carTable)
-        $carTable.get().appendChild(newCar())
-    }
-
-    function newCar(){
-        var $fragment = doc.createDocumentFragment()
-        var $newRow = doc.createElement('tr')
-        var $imagemCell = doc.createElement('td')
-        var $modeloCell = doc.createElement('td')
-        var $anoCell = doc.createElement('td')
-        var $placaCell = doc.createElement('td')
-        var $corCell = doc.createElement('td')
-
-        $imagemCell.textContent =  DOM('[data-js=imagem]').get().value
-        $modeloCell.textContent =  DOM('[data-js=modelo]').get().value
-        $anoCell.textContent =  DOM('[data-js=ano]').get().value
-        $placaCell.textContent =  DOM('[data-js=placa]').get().value
-        $corCell.textContent = DOM('[data-js=cor]').get().value
-
-        $newRow.appendChild($imagemCell)
-        $newRow.appendChild($modeloCell)
-        $newRow.appendChild($anoCell)
-        $newRow.appendChild($placaCell)
-        $newRow.appendChild($corCell)
-        
-        return $fragment.appendChild($newRow)
-    }
-
-    function getCompanyData(){
-        ajax.open('GET','company.json')
-        ajax.send()
-        ajax.addEventListener('readystatechange', handleReadyEventChange,false)
-    }
-
-    function setCompanyData(){
-        var parsedData = JSON.parse(ajax.responseText)
-        $companyName.get().textContent = parsedData.name
-        $companyPhone.get().textContent = parsedData.phone 
-    }
-
-    getCompanyData()
-
-    function handleReadyEventChange(){
-        if(isRequestOk() ) {
-          setCompanyData()
-        }
-        
-      }
-
-      function isRequestOk(){
-        return ajax.readyState === 4 && ajax.status === 200
-      }
-
+  var app =(
+    function () {
+      return {
+        init: function () {
+          this.getCompanyData();
+          this.initEvents();
+        },
   
-  })(window.DOM, window, document);
+        getCompanyData: function getCompanyData() {
+          var ajax = new XMLHttpRequest();
   
+          ajax.open("GET", "company.json");
+          ajax.send();
+          ajax.addEventListener("readystatechange", this.setCompanyData, false);
+        },
+  
+        setCompanyData: function setCompanyData() {
+          if (app.isRequestOk.call(this)) {
+            var $companyName = DOM("[data-js=companyName]");
+            var $companyPhone = DOM("[data-js=companyPhone]");
+            var parsedData = JSON.parse(this.responseText);
+            $companyName.get().textContent = parsedData.name;
+            $companyPhone.get().textContent = parsedData.phone;
+          }
+        },
+  
+        isRequestOk: function isRequestOk() {
+          return this.readyState === 4 && this.status === 200;
+        },
+  
+        initEvents: function initEvents () {
+          var $carForm = DOM("[data-js=carForm]");
+          $carForm.on("submit", this.handleSubmit);
+        },
+  
+        handleSubmit: function handleSubmit(event) {
+          event.preventDefault();
+          var $carTable = DOM("[data-js=carTable]");
+          $carTable.get().appendChild(app.newCar());
+        },
+  
+        newCar: function newCar() {
+          var $fragment = doc.createDocumentFragment();
+          var $newRow = doc.createElement("tr");
+          var $imagemCell = doc.createElement("td");
+          var $modeloCell = doc.createElement("td");
+          var $anoCell = doc.createElement("td");
+          var $placaCell = doc.createElement("td");
+          var $corCell = doc.createElement("td");
+  
+          $imagemCell.textContent = DOM("[data-js=imagem]").get().value;
+          $modeloCell.textContent = DOM("[data-js=modelo]").get().value;
+          $anoCell.textContent = DOM("[data-js=ano]").get().value;
+          $placaCell.textContent = DOM("[data-js=placa]").get().value;
+          $corCell.textContent = DOM("[data-js=cor]").get().value;
+  
+          $newRow.appendChild($imagemCell);
+          $newRow.appendChild($modeloCell);
+          $newRow.appendChild($anoCell);
+          $newRow.appendChild($placaCell);
+          $newRow.appendChild($corCell);
+  
+          return $fragment.appendChild($newRow);
+        },
+      };
+    }
+  )()
+  
+
+  app.init();
+})(window.DOM, window, document);
